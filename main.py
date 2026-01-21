d610b58c46b98f5ecf06b9f61bdf409ab075ac3e
mbgardin <monte.b.gardiner@gmail.com>
2026-01-21 14:55:53 -0700

    ready

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from myproject!")
+import httpx
 
+def main() -> None:
+    r = httpx.get(
+        "https://icanhazdadjoke.com/",
+        headers={"Accept": "application/json"},
+        timeout=10,
+    )
+    r.raise_for_status()
+    print(r.json()["joke"])
 
 if __name__ == "__main__":
     main()
